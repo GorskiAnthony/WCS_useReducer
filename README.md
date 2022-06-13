8c2603243b1da4ae3ad6bef79f72309b5c88ff58
Anthony Gorski <anthony.gorski@wildcodeschool.com>
2022-06-13 10:47:50 +0200

    Add todo with done with reducer

--- README.md
+++ README.md
@@ -15,3 +15,56 @@ cd <projet>
 npm install
 npm start
 ```
+
+## Étape 1 - Création du reducer
+
+Nous allons donc changer quelques éléments du code de l'application.
+Dans `App.jsx` nous allons faire les modifications suivantes :
+
+```diff
+- const [todos, setTodos] = useState(datas);
++ const initialValue = datas;
+```
+
+Nous allons ajouter le reducer à notre application.
+```js
+const [todos, dispatchTodo] = useReducer(todoReducer, initialValue);
+```
+
+Mais la fonction `todoReducer` existe t'elle ? Non, nous devons donc la créer.
+
+```js
+// Notre fonction "reducer" prenant en argument
+// `state` la valeur actuelle du `useReducer et
+// action l'argument passé par la fonction `dispatchTodo`
+const todoReducer = (state, action) => {
+    // On se basera sur la propriété `type` de l'objet envoyé par `dispatchTodo`
+    // pour déterminer l'action à effectuer sur le `state`
+    switch (action.type) {
+        // si action.type === 'ADD_TODO', on ajoute la propriété todo au `state`
+        case "ADD_TODO":
+            return [...state, action.payload];
+        // si action.type === 'REMOVE_TODO', on retire l'équivalent de la propriété todo du `state`
+        case "REMOVE_TODO":
+            return state.filter((todo) => todo !== action.payload);
+        // Sinon retourner la valeur actuelle (cela aura pour conséquence de ne pas réafficher le composant)
+        default:
+            return state;
+    }
+}
+```
+
+## Étape 2 : Ajout de la méthode "addTodo"
+
+```js
+const addTodo = () => {
+    dispatchTodo({ type: "ADD_TODO", payload: input });
+    setInput("");
+};
+```
+
+Du coup, nous devons faire une autre modification, sur le bouton Ajouter.
+
+```js
+<button onClick={addTodo}>Ajouter</button>
+```
--- src/page/App.jsx
+++ src/page/App.jsx
@@ -1,19 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 import "../assets/css/App.css";
 import datas from "../helpers/data.js";
 import TodoList from "../components/TodoList";
 
 const App = () => {
-  const [todos, setTodos] = useState(datas);
   const [input, setInput] = useState("");
+  /**
+   * Nous allons donc modifier le code de notre composant TodoList pour qu'il prenne en compte notre state.
+   * const [todos, setTodos] = useState(datas);
+   */
+  const initialValue = datas;
+
+  /**
+   * Nous allons créer notre todoReducer pour gérer notre state.
+   */
+  const todoReducer = (state, action) => {
+    /**
+     *  console.log({ state, action });
+     *  // output:
+     *  action: {type: 'ADD_TODO', payload: 'coucou'}
+     *  state: (2) ['hello', 'world!']
+     */
+    // console.log({ state, action });
+    switch (action.type) {
+      case "ADD_TODO":
+        return [...state, action.payload];
+      case "REMOVE_TODO":
+        return state.filter((todo) => todo !== action.payload);
+      default:
+        return state;
+    }
+  };
+
+  const [todos, dispatchTodo] = useReducer(todoReducer, initialValue);
 
   const handleChange = (e) => {
     setInput(e.target.value);
   };
 
-  const handleClick = (e) => {
-    e.preventDefault();
-    setTodos([...todos, input]);
+  const addTodo = () => {
+    dispatchTodo({ type: "ADD_TODO", payload: input });
     setInput("");
   };
 
@@ -22,7 +48,7 @@ const App = () => {
       <h1>To do</h1>
       <div>
         <input type="text" value={input} onChange={handleChange} />
-        <button onClick={handleClick}>Ajouter</button>
+        <button onClick={addTodo}>Ajouter</button>
       </div>
       <TodoList todos={todos} />
     </div>
